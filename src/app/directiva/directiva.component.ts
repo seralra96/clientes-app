0edfd44bf94aff3f4cbadea9e68ccbccca048fd0
Sergio Ramírez <seralra96@gmail.com>
2019-03-18 21:38:51 -0500

    Curso Angular Sección 2 Clase 12

---
+++ src/app/directiva/directiva.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-directiva',
+  templateUrl: './directiva.component.html'
+})
+export class DirectivaComponent {
+
+  listaCurso: string[] = ['TypeScript', 'JavaScript','Java SE', 'C#', 'PHP']
+  constructor() { }
+
+
+}
