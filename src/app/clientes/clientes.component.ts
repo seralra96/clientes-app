2acb32d359380d8650e9e4b157a887e0397170ee
Sergio Ramírez <seralra96@gmail.com>
2019-03-19 21:01:49 -0500

    Curso Angular Sección 3 Clase 16

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
+import { CLIENTES } from './clientes.json';
 
 @Component({
   selector: 'app-clientes',
@@ -7,23 +8,12 @@ import { Cliente } from './cliente';
 })
 export class ClientesComponent implements OnInit {
 
-  clientes: Cliente[] = [
-    {id: 1, nombre: 'Sergio', apellido: 'Ramírez', email: 'seralra96@hotmail.com', createAt: '2019-03-18'},
-    {id: 2, nombre: 'Sergio2', apellido: 'Ramírez', email: 'seralra962@hotmail.com', createAt: '2019-03-18'},
-    {id: 3, nombre: 'Sergio3', apellido: 'Ramírez', email: 'seralra963@hotmail.com', createAt: '2019-03-18'},
-    {id: 4, nombre: 'Sergio4', apellido: 'Ramírez', email: 'seralra964@hotmail.com', createAt: '2019-03-18'},
-    {id: 5, nombre: 'Sergio5', apellido: 'Ramírez', email: 'seralra965@hotmail.com', createAt: '2019-03-18'},
-    {id: 6, nombre: 'Sergio6', apellido: 'Ramírez', email: 'seralra966@hotmail.com', createAt: '2019-03-18'},
-    {id: 7, nombre: 'Sergio7', apellido: 'Ramírez', email: 'seralra967@hotmail.com', createAt: '2019-03-18'},
-    {id: 8, nombre: 'Sergio8', apellido: 'Ramírez', email: 'seralra968@hotmail.com', createAt: '2019-03-18'},
-    {id: 9, nombre: 'Sergio9', apellido: 'Ramírez', email: 'seralra969@hotmail.com', createAt: '2019-03-18'},
-    {id: 10, nombre: 'Sergio10', apellido: 'Ramírez', email: 'seralra9610@hotmail.com', createAt: '2019-03-18'},
-    {id: 11, nombre: 'Sergio11', apellido: 'Ramírez', email: 'seralra9611@hotmail.com', createAt: '2019-03-18'}
-  ];
+  clientes: Cliente[];
 
   constructor() { }
 
   ngOnInit() {
+    this.clientes = CLIENTES
   }
 
 }
